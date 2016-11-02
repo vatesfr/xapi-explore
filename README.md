53545a05ccef584d7d2bfb5af95137158f4206f7
Julien Fontanet <julien.fontanet@isonoe.net>
2016-11-02 10:34:23 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,49 @@
+# xapi-explore [![Build Status](https://travis-ci.org/vatesfr/xapi-explore.png?branch=master)](https://travis-ci.org/vatesfr/xapi-explore)
+
+> Interactive exploration of the Xen API.
+
+## Install
+
+Installation of the [npm package](https://npmjs.org/package/xapi-explore):
+
+```
+> npm install --save xapi-explore
+```
+
+## Usage
+
+**TODO**
+
+## Development
+
+```
+# Install dependencies
+> npm install
+
+# Run the tests
+> npm test
+
+# Continuously compile
+> npm run dev
+
+# Continuously run the tests
+> npm run dev-test
+
+# Build for production (automatically called by npm install)
+> npm run build
+```
+
+## Contributions
+
+Contributions are *very* welcomed, either on the documentation or on
+the code.
+
+You may:
+
+- report any [issue](https://github.com/vatesfr/xapi-explore/issues)
+  you've encountered;
+- fork and create a pull request.
+
+## License
+
+ISC © [Vates SAS](https://vates.fr)
---
+++ src/index.js
@@ -0,0 +1,101 @@
+import blessed from 'blessed'
+import execPromise from 'exec-promise'
+import minimist from 'minimist'
+import pw from 'pw'
+import { createClient } from 'xen-api'
+import { forEach, map } from 'lodash'
+import { name, version } from '../package.json'
+
+// ===================================================================
+
+const askPassword = prompt => new Promise(resolve => {
+  prompt && process.stderr.write(`${prompt}: `)
+  pw(resolve)
+})
+
+const required = name => {
+  const e = `missing required argument <${name}>`
+  throw e
+}
+
+// -------------------------------------------------------------------
+
+execPromise(async args => {
+  const opts = minimist(args, {
+    boolean: [ 'help', 'version' ]
+  })
+
+  if (opts.help) {
+    return `Usage: ${name} <Xen URL> <Xen user> [<Xen password>]`
+  }
+
+  if (opts.version) {
+    return version
+  }
+
+  const [
+    url = required('Xen URL'),
+    user = required('Xen user'),
+    password = await askPassword('Xen password')
+  ] = opts._
+
+  const xapi = createClient({
+    auth: { user, password },
+    url
+  })
+
+  console.log('connecting to %s…', url)
+  await xapi.connect()
+
+  const screen = blessed.Screen({
+    smartCSR: true
+  })
+  screen.key([ 'escape', 'q', 'C-c' ], () => {
+    screen.destroy()
+  })
+
+  const list = blessed.list({
+    border: {
+      type: 'line'
+    },
+    keys: true,
+    mouse: true,
+    parent: screen,
+    style: {
+      bg: 'black',
+      fg: 'white',
+      selected: {
+        bg: 'white',
+        fg: 'black'
+      }
+    }
+  })
+
+  xapi.objects.on('finish', items => {
+    const byType = {}
+    forEach(xapi.objects.all, item => {
+      const nameLabel = item.name_label
+
+      if (nameLabel) {
+        const type = item.$type
+        ;(byType[type] || (byType[type] = [])).push(nameLabel)
+      }
+    })
+
+    const listItems = []
+    forEach(Object.keys(byType).sort(), type => {
+      listItems.push(`${type}/`)
+      forEach(byType[type].sort(), item => {
+        listItems.push(` - ${item}`)
+      })
+    })
+    list.setItems(listItems)
+
+    list.select(0)
+    list.screen.render()
+  })
+
+  screen.render()
+  await new Promise(resolve => screen.on('destroy', resolve))
+  console.log('bye :-)')
+})
